ce64f5faba9c220542bdacb85369a7a7310f8897
jinnliong <chin.jinnliong@gmail.com>
2024-02-23 20:35:48 +0800

    add richie's .tf files

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
-resource "aws_s3_bucket" "example" {
+module "vpc" {
+  source = "./modules/vpc"
 
-  bucket = "jl-terraform-ci-bucket"
-}
-
-# An example resource that does nothing.
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
-  }
+  cidr_block           = "10.0.0.0/16"
+  name_prefix          = "sctp-ce4-group1"
+  public_subnet_count  = 3
+  private_subnet_count = 3
+  create_natgw         = true
 }
