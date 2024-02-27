70a207176ad98bdba1b16e769cef665cc4f424cb
jinnliong <chin.jinnliong@gmail.com>
2024-02-27 19:40:37 +0800

    update readme

--- envs/dev/ec2/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "aws_region" {
-  description = "The AWS region to deploy into"
-  type        = string
-  default     = "ap-northeast-1" # Optional: provide a default value
-}
--- envs/prod/ec2/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  # The region is automatically determined from the AWS_DEFAULT_REGION environment variable
-  region = var.aws_region
-}
