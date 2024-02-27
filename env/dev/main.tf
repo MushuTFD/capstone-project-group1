85f4d48522fad1ec7b2f3842656ce9b69d894114
MushuTFD <lwyiscool@gmail.com>
2024-02-27 22:54:13 +0800

    update branch

---
+++ env/dev/main.tf
@@ -0,0 +1,20 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  cidr_block = "10.0.0.0/16"
+  name_prefix = "${local.name_prefix}"
+  public_subnet_count = 3
+  private_subnet_count = 3
+  create_natgw = true
+}
+module "alb" {
+  source = "../../modules/alb"
+  name_prefix = "${local.name_prefix}"
+
+  all_subnets = module.vpc.public_subnets
+  vpc_id = module.vpc.vpc_id
+}
+
+module "route53" {
+    source = "../../modules/routes53"
+}
---
+++ env/prod/versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.6"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.31.0"
+    }
+  }
+cloud {
+  organization = "weiyang-org"
+
+  workspaces {
+    name = "capstone-project-prod"
+  }
+}
+}
