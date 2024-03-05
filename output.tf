a1623ae47e1e3d57a6236f6925d9f6e98e5777ea
jinnliong <chin.jinnliong@gmail.com>
2024-03-05 20:36:05 +0800

    merge to main branch

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "image_name" {
-  value = module.ecs.image_name
+  value = "${module.ecs.image_name}"
 }
 
 output "flask_lb_dns_name" {
-  value = module.alb_flask.load_balancer_dns_name
+  value = "${module.alb_flask.load_balancer_dns_name}"
 }
 
 output "react_lb_dns_name" {
-  value = module.alb_react.load_balancer_dns_name
+  value = "${module.alb_react.load_balancer_dns_name}"
 }
