36c4be4766ff3dcac2c86a56b95fc9f2d286874c
jinnliong <chin.jinnliong@gmail.com>
2024-03-05 19:13:04 +0800

    simplify terraform init

--- locals.tf
+++ locals.tf
@@ -1,3 +1,9 @@
 locals {
-  name_prefix = "sctp-ce4-group1"
+  name_prefix      = "sctp-ce4-group1"
+  frontend_service = "react-1"
+  backend_service  = "flask"
+
+  #Must've created le
+  frontend_repository = "richie-react-app"
+  backend_repository  = "richie-flask-app"
 }
